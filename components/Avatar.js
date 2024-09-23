b36976432f38e926004a01cfa09b26b27961a279
aDevMister <herbdolqdus@gmail.com>
2024-09-23 18:23:13 +0100

    index page

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className="hidden xl:flex xl:max-w-none ">
+    <Image
+      src={"/avatar.png"}
+      width={737}
+      height={678}
+      alt="Profile picture"
+      className="translate-z-0 w-full h-full "
+    />
+    {/* <div className="absolute top-0 left-0 w-[200px] h-[200px] bg-primary/100 rounded-full z-10"></div> */}
+  </div>;
 };
 
 export default Avatar;
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <>
+      <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
+        <div className="container mx-auto">
+          <div>
+            <Link href={"/"} className="flex items-center gap-5 ">
+              <h1 className="text-white text-4xl font-bold">AbdulQudus </h1>
+              <h1 className="text-accent text-4xl ">Ajape </h1>
+            </Link>
+            <Socials />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
