aaf999589d190e221f14e7e9074706d60c1c034a
aDevMister <herbdolqdus@gmail.com>
2024-09-29 18:11:38 +0100

    my service

--- components/Nav.js
+++ components/Nav.js
@@ -32,7 +32,7 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 xl:w-16 xl:max-w-md xl:h-screen">
+    <nav className="flex xl:flex-col items-center xl:justify-center fixed bottom-0 w-full  xl:right-[2%] xl:top-0 xl:h-screen  gap-y-4   h-max  mt-auto  z-10 top-0 xl:w-16 xl:max-w-md">
       <div className=" flex w-full px-4 xl:flex-col items-center justify-between xl:justify-cente gap-x-10 gap-y-10  md:px-40 xl:px-0 h-[80px] bg-white/10 xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {
           return (
--- pages/about/index.js
+++ pages/about/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
+import CountUp from "react-countup";
 // icons
 import {
   FaHtml5,
@@ -28,18 +29,18 @@ const aboutData = [
       {
         title: "Web Development",
         icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          FaHtml5,
+          FaCss3,
+          FaJs,
+          FaReact,
+          SiNextdotjs,
+          SiFramer,
+          FaWordpress,
         ],
       },
       {
         title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [FaFigma, SiAdobexd, SiAdobephotoshop],
       },
     ],
   },
@@ -93,33 +94,103 @@ const aboutData = [
 ];
 
 const About = () => {
-  // Corrected useState
   const [index, setIndex] = useState(0);
 
   return (
-    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+    <div className="h-[100vh] bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
-      <motion.div
+      {/*  <motion.div
         variants={fadeIn("right", 0.2)}
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6"
+        className="container h-full mx-auto flex flex-col items-center xl:flex-row gap-x-6"
       >
         <Avatar />
-      </motion.div>
+      </motion.div> */}
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
-        <div>
+        <div className="flex-1 flex flex-col justify-center ">
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Captivating <span className="text-accent  ">stories</span> birth
+            magnificent designs.
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 "
+          >
+            2 years ago, i began freelancing as a developer. since then, I&apos;ve
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use
+          </motion.p>
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8 "
+          >
+            <div className="flex flex-1 xl:gap-x-6 ">
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={2} duration={10} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ">
+                  Years of Experience
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={250} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ">
+                  Satisfied clients
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={650} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ">
+                  Finished projects
+                </div>
+              </div>
+              <div className="relative flex-1  ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={8} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ">
+                  Winning awards
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px] "
+        >
           <div className="flex gap-x-4 xl:gap-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
                 <div
                   key={itemIndex}
-                  className={`${
-                    index === itemIndex &&
-                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
-                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0`}
+                  className={`cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0 ${
+                    index === itemIndex
+                      ? "text-accent after:w-[100%] after:bg-accent"
+                      : ""
+                  }`}
                   onClick={() => setIndex(itemIndex)}
                 >
                   {item.title}
@@ -127,7 +198,29 @@ const About = () => {
               );
             })}
           </div>
-        </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flax-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60  "
+                >
+                  <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
+                  {item.stage && <div>{item.stage}</div>}
+                  <div className="flex gap-x-4">
+                    {item.icons?.map((Icon, iconIndex) => {
+                      return (
+                        <div key={iconIndex} className="text-2xl text-white">
+                          <Icon />
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
       </div>
     </div>
   );
